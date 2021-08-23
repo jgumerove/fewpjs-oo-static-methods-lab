1d1510a491b46a3ec47019b984bba7b05dd56ed7
jgumerove <josh.gumerove@gmail.com>
2021-08-23 15:12:40 -0400

    commit tree 3a945717e716d8dfbd3130bb6b601c397dbf850a

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let firstWord = this.capitalize(string).split(" ")[0]
+    let words = string.split(" ").slice(1)
+    let final = words.map(word => {
+      if(word === "the" || word === "a" || word === "an" || word === "but" || word === "of" || word === "and" || word ==="for" || word === "at" || word === "by" || word === "from") {
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    })
+    return (firstWord + " " + final.join(" "))
+  }
+
 }
+
+
